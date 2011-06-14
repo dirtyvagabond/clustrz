ba3c320469ca4b96c3f09aac7a21a876d8fe2e01
Aaron Crow <aaroncrow@Aaron-Crows-MacBook-Air.local>
2011-06-14 09:14:53 -0700

    adding source

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clustrz "1.0.0-SNAPSHOT"
+  :description "My little cluster project"
+  :dependencies [[org.clojure/clojure "1.3.0-alpha6"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-ssh "0.2.4"]
+                ]
+  :dev-dependencies [
+    [swank-clojure "1.4.0-SNAPSHOT"]
+])
---
+++ test/upstart/test/core.clj
@@ -0,0 +1,6 @@
+(ns upstart.test.core
+  (:use [upstart.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

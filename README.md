0f8eacd1964a3b6cbab807c36e16367d29612e1f
Aaron Crow <dirtyvagabond@yahoo.com>
2011-07-07 15:18:41 -0700

    added better docs

--- README.md
+++ README.md
@@ -1,30 +1,21 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1038\cocoasubrtf350
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww9000\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\ql\qnatural\pardirnatural
+# clustrz
 
-\f0\fs24 \cf0 # clustrz\
-\
-Clustrz provides a flexible library for handling remote Linux processes.\
-\
-Clustrz lets you define remote nodes easily, which you can then group into logical clusters. You then have a rich set of functions for inspecting, managing, and otherwise taming these nodes and clusters.\
-\
-Among other convenience functions, clustrz provides easy ways to interact with JMX-enabled processes.\
-\
-Higher order functions can be written to perform remote actions on individual nodes. Those functions may then be run against all nodes in a cluster in parallel.\
-\
-You must have your public key on each node that you wish to work with.\
-\
-## Usage\
-\
-First, define your nodes. The minimum required is the remote username and the remote host name or ip address, like so:\
-\
-	> (def mynode \{:host "some_hostname" :user "some_user"\})\
-\
-Now you can use various functions with that node. Let's take a look at clustrz functions, starting with the lowest level and working our way up:\
-\
-	> \
-\
-\
-}
+Clustrz provides a flexible library for handling remote Linux processes.
+
+Clustrz lets you define remote nodes easily, which you can then group into logical clusters. You then have a rich set of functions for inspecting, managing, and otherwise taming these nodes and clusters.
+
+Among other convenience functions, clustrz provides easy ways to interact with JMX-enabled processes.
+
+Higher order functions can be written to perform remote actions on individual nodes. Those functions may then be run against all nodes in a cluster in parallel.
+
+You must have your public key on each node that you wish to work with.
+
+## Usage
+
+First, define your nodes. The minimum required is the remote username and the remote host name or ip address, like so:
+
+	> (def mynode {:host "some_hostname" :user "some_user"})
+
+Now you can use various functions with that node. Let's take a look at clustrz functions, starting with the lowest level and working our way up:
+
+	> 
--- src/clustrz/core.clj
+++ src/clustrz/core.clj
@@ -1,3 +1,14 @@
+;;
+;; Provides a flexible library for managing remote Linux environments.
+;; Treats each environment as a simple "node", and supports grouping
+;; nodes into "clusters".
+;;
+;; Higher order functions can be written to perform remote actions on
+;; a node, and then those functions may be run against individual nodes,
+;; or against all nodes in a cluster in parallel.
+;;
+;; You must have your public key on each node that you wish to work with.
+;;
 (ns clustrz.core
   (:use [clojure.string :only (split split-lines blank? join trim trim-newline)])
   (:use [clojure.java.shell :only (sh)])
@@ -7,15 +18,19 @@
   (:require [clojure.contrib.jmx :as jmx])
   (:gen-class))
 
+;;; Defines where clustrz stores certain data on nodes.
 (def *home* "~/.clustrz/")
 (def *kvs-dir* (str *home* "kvs/"))
 (def *log* (str *home* "node.log"))
 
 (defn now [] (java.util.Date.))
 
-;; TODO: consider security; "bash injection" attacks,
-;;       e.g., passing hostile escaped bash code to things like assoc-at.
-(defn ssh-exec [{:keys [host user]} cmd]
+;;; TODO: consider security; "bash injection" attacks,
+;;;       e.g., passing hostile escaped bash code to things like assoc-at.
+(defn ssh-exec
+  "Runs cmd on the specified node and returns a hashmap of the
+   remote result of running the command."
+  [{:keys [host user]} cmd]
   (sh "ssh" (str user "@" host) cmd))
 
 (defn shout
@@ -28,46 +43,63 @@
       (trim out)
       (throw (Exception. (str "shout error: exit=" exit ", err=\"" (trim-newline err) "\""))))))
 
-(defn ps-map [line]
+(defn ps-map
+  "Utility function to parse the textual line output of our custom ps command.
+   Returns a hashmap representing the remote process data parsed from the line."
+  [line]
   (let [[pid time pctcpu cmd] (split line #"\|")]
     {:pid (Long/parseLong (trim pid))
      :time (trim time)
      :pctcpu (Float/parseFloat (trim pctcpu))
      :cmd cmd}))
 
-(defn ps [node]
-  "Fetches data about running processes at node. Returns a list of hash maps. Each
+(defn ps
+  "Fetches data about running processes at node. Returns a list of hashmaps. Each
    hashmap in the list represents a running process. Keys of the map include:
      :pid
      :time
      :pctcpu
      :cmd"
+  [node]
   (let [cmd (str "ps --no-header -u " (:user node) " -o \"%p|%x|%C|%a\"")
         lines (split-lines (shout node cmd))]
     (map #(ps-map %) lines)))
 
-(defn mkdir-at [node file]
-  (ssh-exec node (str "mkdir -p " file)))
+(defn mkdir-at
+  "Creates the specified path at node. All subdirectories will be created if
+   the don't already exist."
+  [node path]
+  (ssh-exec node (str "mkdir -p " path)))
 
-(defn delete-file-at [node file]
+(defn delete-file-at
+  "Deletes the specified file at node."
+  [node file]
   (ssh-exec node (str "rm " file)))
 
-(defn last-lines [node file n]
+(defn last-lines
+  "Returns the last n lines from file at node."
+  [node file n]
   (shout node (str "tail -" n " " file)))
 
-(defn last-line [node file]
+(defn last-line
+  "Returns the last line from file at node."
+  [node file]
   (last-lines node file 1))
 
 ;; TODO: accidentally trying (execs f a-node) results in opaque error.
 ;;       should i have just one exec, that asks (seq? nodes) ?
 ;;       or at least put a precondition on execs that nodes be a seq?
 
-(defn exec [f node]
-  "Runs f against node, and wraps the result in a hash map that contains
+(defn exec
+  "Runs f against node, and wraps the result in a hashmap that contains
    extra information about the run. The result of f itself will be at the
-   key :out. Other keys include:
-   :host  the node's hostname
-   :time  how long the run took"
+   key :out.
+
+   Keys in the returned hashmap include:
+     :out  the result of running f on node
+     :host  the node's hostname
+     :time  how long the run took"
+  [f node]
   (let [start (System/currentTimeMillis)
         out (trim (f node))
         t (- (System/currentTimeMillis) start)]
@@ -75,15 +107,20 @@
      :host  (node :host)
      :time  t}))
 
-(defn execs [f nodes]
+(defn execs
+  "Runs f against all nodes in parallel. Returns a sequence of results,
+   where each element in the sequence is a result from running f on one
+   of the nodes."
+  [f nodes]
   (doall (apply pcalls (map #(partial f %) nodes))))
 
 (defn uptime-at
- "Fetches the raw uptime string from node."
+ "Returns the raw uptime string from node."
  [node]
  (shout node "uptime"))
 
-(defn nice-report-str [hashmaps]
+(defn nice-report-str
+  [hashmaps]
   (join "\n" (map #(str (:host %) ": " (:out %)) hashmaps)))
 
 (defn nice-seq [thing]
@@ -97,7 +134,10 @@
 ;;      function that expects nodes, like the exec and execs
 ;;      functions... this can get tedious when writing calling
 ;;      code.
-(defn $ [f nodes]
+(defn $
+  "Runs f across all nodes in parallel. Each result is wrapped in
+   a hashmap per our custom exec function, and returned in a sequence."
+  [f nodes]
   (doall (apply pcalls (map #(partial exec f %) (nice-seq nodes)))))
 
 (defn report [fn nodes]
@@ -119,14 +159,19 @@
   (let [dest (str user "@" host ":" dest-path)]
     (apply sh (flatten ["scp" files dest]))))
 
-(defn spit-at [node dest-file val]
+(defn spit-at
+  "Puts the textual representation of val in dest-file at node.
+   The file will be overwritten if it already exists."
+  [node dest-file val]
   (let [tmp-local-file (tmp-file)]
     (spit tmp-local-file (str val))
     (let [res (copy-to tmp-local-file node dest-file)]
       (delete-file tmp-local-file)
       res)))
 
-(defn append-spit-at [node dest-file s]
+(defn append-spit-at
+  "Appends the textual representation of s to dest-file at node."
+  [node dest-file s]
   (let [tmp-dest-file (tmp-file)]
     (spit-at node tmp-dest-file s)
     (ssh-exec node (str "cat " tmp-dest-file " >> " dest-file "; rm " tmp-dest-file))))
@@ -140,7 +185,7 @@
 (defn assoc-at
   "Associates val with key, at node. val can be any Clojure object."
   [node key val]
-  ;;optimize: mkdir is only needed once per node, and only if the dir isn't there. how to track?
+  ;;; OPTIMIZE: mkdir is only needed once per node, and only if the dir isn't there. how to track?
   (mkdir-at node *kvs-dir*)
   (spit-at node (kvs-file key) (with-out-str (pr val))))
 
@@ -155,10 +200,12 @@
      "Returns the object associated with key at node, or nil if none."
      (get-at node key nil)))
 
-(defn dissoc-at [node key]
+(defn dissoc-at
+  "Removes key at node."
+  [node key]
   (delete-file-at node (kvs-file key)))
 
-;; TODO: creates a DateFormat each time for thread safety. better way?
+;;; TODO: creates a DateFormat each time for thread safety. better way?
 (defn bash-time
   "Converts a bash time string to a java Date.
    Example input, t: 'Fri Dec 3 02:51:12 PST 2010'"
@@ -166,10 +213,14 @@
   (let [df (java.text.SimpleDateFormat. "EEE MMM d HH:mm:ss z yyyyy")]
     (.parse df t)))
 
-(defn slurp-at [node file]
+(defn slurp-at
+  [node file]
+  "Returns the contents of file at node."
   (shout node (str "cat " file)))
 
-(defn log-at [node msg]
+(defn log-at
+  [node msg]
+  "Appends msg to the central log stored at node."
   (append-spit-at node *log* (str (now) ": " msg "\n")))
 
 (defn log [msg]
@@ -179,17 +230,26 @@
   (log-at node msg)
   (log (str (node :host) ": " msg)))
 
-(defn up? [{:keys [user host pid] :as node}]
+(defn up?
+  [{:keys [user host pid] :as node}]
+  "Returns true if and only if the specified remote process is
+   running."
   (let [out (shout node (str "ps --no-header -p " pid))]
     (not (blank? out))))
 
 (def down?
   (complement up?))
 
-(defn java? [proc]
+(defn java?
+  [proc]
+  "Returns true if and only if the specified remote
+   process is a Java process."
   (not (nil? (re-matches #".*java" (first (split (proc :cmd) #"\s"))))))
 
-(defn clojure? [proc]
+(defn clojure?
+  [proc]
+  "Returns true if and only if the specified remote process is a
+   Clojure process."
   (not (nil? (re-matches #".* clojure\.main .*" (proc :cmd)))))
 
 (defn chmod-at [node opts file]
@@ -197,16 +257,17 @@
 
 (defn wget-at
   ([node url dest-dir]
-    (wget-at node url dest-dir ""))
+     "Runs wget at node for the specified url, with pwd set to dest-dir."
+     (wget-at node url dest-dir ""))
   ([node url dest-dir opts]
      "Runs wget at node for the specified url, with pwd set to dest-dir.
       opts must be valid options as one string, or an empty string."
-    (ssh-exec node (str "cd " dest-dir "; wget " opts " " url))))
+     (ssh-exec node (str "cd " dest-dir "; wget " opts " " url))))
 
 (defn tagger
-  "Associates key to node, where the value of key is the
-   result of calling f on node."
   [node key f]
+  "Associates key to node, where the value of key is the result of
+   calling f on node."
   (assoc node key (f node)))
 
 (defn >+ [f nodes]
@@ -224,35 +285,53 @@
    :environment {"jmx.remote.credentials" (into-array [(get-in node [:jmx :user])
                                                        (get-in node [:jmx :pwd])])}})
 
-(defn jmx-names [node]
+(defn jmx-names
+  [node]
+  "Returns a sequence of JMX ObjectNames, where each ObjectName
+   represents a JMX mbean available at node. This can be used to
+   discover the full set of mbeans available at node."
   (into #{}
    (jmx/with-connection (jmx-props node)
      (jmx/mbean-names "*:*"))))
 
 (defn jmx-type-at
   ([node package type]
-    (jmx/with-connection (jmx-props node)
-      (jmx/mbean (str package ":type=" type))))
+     "Returns the JMX mbean available at node for type,
+      under package."
+     (jmx/with-connection (jmx-props node)
+       (jmx/mbean (str package ":type=" type))))
   ([node type]
+     "Returns the JMX mbean available at node for type,
+      under the java.lang package."
      (jmx-type-at node "java.lang" type)))
 
-(defn start-time-at [node]
+(defn start-time-at
+  [proc]
+  "Returns a Java Date representing the time that the specified remote
+   JMX-enabled process was started."
   (java.util.Date.
     (:StartTime
-      (jmx-type-at node "Runtime"))))
+      (jmx-type-at proc "Runtime"))))
 
-(defn os-at [node]
-  (jmx-type-at node "OperatingSystem"))
+(defn os-at
+  [proc]
+  "Returns the OperatingSystem mbean data for the JMX-enabled proc."
+  (jmx-type-at proc "OperatingSystem"))
 
-(defn load-avg-at [node]
+(defn load-avg-at
+  [proc]
+  "Returns the load average for the OS running the JMX-enabled proc,
+   as a Double."
   (:SystemLoadAverage
-    (os-at node)))
+    (os-at proc)))
 
-(defn threading-at [node]
-  (jmx-type-at node "Threading"))
+(defn threading-at
+  [proc]
+  "Returns the Threading mbean data for the JMX-enabled proc."
+  (jmx-type-at proc "Threading"))
 
 ;;
-;; Quartz specific
+;; Factual/Quartz specific
 ;;
 
 (defn vote-server? [proc]
